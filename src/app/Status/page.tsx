95b47a8ffd4745752b6732382e9fc7840027fb14
DibyashaktIMoharana <dibyacollege@gmail.com>
2024-08-11 21:44:10 +0530

    reverting

--- src/app/Status/page.tsx
+++ src/app/Status/page.tsx
@@ -6,7 +6,7 @@ import React, { useState } from "react";
 const Status = [
   {
     Available: {
-      name: "Completed",
+      name: "Available for work",
       color: "lime",
     },
     NotAvailable: {
@@ -20,53 +20,44 @@ const Status = [
   },
 ];
 
-interface StatusData {
-    name: string;
-    color: string;
+
+
+function Statuspage({ isAvailable, Relaxing, className }: { isAvailable: any, Relaxing: any, className: any }) {
+  let [status, setStatus] = useState(
+    isAvailable
+      ? Status[0].Available
+      : Relaxing
+      ? Status[0].Relaxing
+      : Status[0].NotAvailable
+  );
+
+  let bgColorClass =
+    status === Status[0].Available
+      ? "bg-lime-400"
+      : status === Status[0].Relaxing
+      ? "bg-lime-400 "
+      : "bg-red-600 ";
+
+  return (
+    <div>
+      <span
+        className={`inline-flex items-center px-2 py-0 text-xs font-semibold  pointer-events-auto ${className} `}
+      >
+        {/* green ping circle */}
+        <span className="mr-1.5 flex justify-center items-center h-2 w-2  ">
+          <span
+            className={`absolute flex  w-2 h-2 rounded-full  opacity-75 animate-ping ${bgColorClass}`}
+          ></span>
+          <span
+            className={`relative flex w-1 h-1 rounded-full ${bgColorClass}`}
+          ></span>
+        </span>
+
+        {/* //shows status name */}
+        <span className="dark:text-neutral-500 ">{status.name}</span>
+      </span>
+    </div>
+  );
 }
 
-interface StatusProps {
-    isAvailable: boolean;
-    Relaxing: boolean;
-    className: string;
-}
-
-function StatusPage({ isAvailable, Relaxing, className }: StatusProps) {
-    let [status, setStatus] = useState<StatusData>(
-        isAvailable
-            ? Status[0].Available
-            : Relaxing
-            ? Status[0].Relaxing
-            : Status[0].NotAvailable
-    );
-
-    let bgColorClass =
-        status === Status[0].Available
-            ? "bg-lime-400"
-            : status === Status[0].Relaxing
-            ? "bg-lime-400 "
-            : "bg-red-600 ";
-
-    return (
-        <div>
-            <span
-                className={`inline-flex items-center px-2 py-0 text-xs font-semibold  pointer-events-auto ${className} `}
-            >
-                {/* green ping circle */}
-                <span className="mr-1.5 flex justify-center items-center h-2 w-2  ">
-                    <span
-                        className={`absolute flex  w-2 h-2 rounded-full  opacity-75 animate-ping ${bgColorClass}`}
-                    ></span>
-                    <span
-                        className={`relative flex w-1 h-1 rounded-full ${bgColorClass}`}
-                    ></span>
-                </span>
-
-                {/* //shows status name */}
-                <span className="dark:text-neutral-500 ">{status.name}</span>
-            </span>
-        </div>
-    );
-}
-
-export default StatusPage;
+export default Statuspage;
